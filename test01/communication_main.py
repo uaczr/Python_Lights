fd9739697d553438e1568d9b4ea38010187ec6c2
Simon Fritz <simon-fritz@gmx.de>
2017-04-22 02:45:19 -0700

    Initial Commmit

---
+++ test01/communication_main.py
@@ -0,0 +1,35 @@
+#################################################################
+# Autor: Simon
+# date: 2017-04-05
+# Name: main_communication
+# Usage: sets up communication and provides all main communication functions
+#################################################################
+
+"""def main():
+
+
+# starts mosquito and mqtt-server
+def start_server():
+    #################################################################
+    # Autor: Simon
+    # Name: Mqtt-Test
+    # Usage: submits a "test" Topib
+    #################################################################
+
+    # Start-Up-Script
+    # http://askubuntu.com/questions/814/how-to-run-scripts-on-start-up
+
+    # Server starten!!!!
+    # mosquitto in conspole
+
+    # Multi-Threading
+    # https://www.tutorialspoint.com/python/python_multithreading.htm
+
+    # import paho.mqtt.client as client
+    # import time
+    import paho.mqtt.publish as publish
+
+    def start_mqtt()
+        topicName = "test"
+        publish.single(topicName, "mqtt^^", hostname="localhost")
+        print("..topic: " + topicName + " refreshed")"""
---
+++ test01/light_main.py
@@ -0,0 +1,8 @@
+#################################################################
+# Autor: Simon
+# date: 2017-04-05
+# Name: light_main
+# Usage: sets up communication and provides all main communication functions
+#################################################################
+
+"""def main():"""
