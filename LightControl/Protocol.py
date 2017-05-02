078b9763a4ae327e6db364c6e3d316fecd1104d9
Simon Fritz <simon-fritz@gmx.de>
2017-05-02 15:41:23 -0700

    Folder Rework

---
+++ LightControl/Protocol.py
@@ -0,0 +1,38 @@
+class pushingMessage:
+    messageType = ' '
+    pushing1 = ' '
+    pushing2 = ' '
+    pushing3 = ' '
+    pushing4 = ' '
+    pushing5 = ' '
+    pushing6 = ' '
+
+class settingMessage:
+    messageType = ' '
+    basePattern = ' '
+    baseColor = ' '
+    baseSpeed = ' '
+    baseBrightness = ' '
+    frontPattern = ' '
+    frontColor = ' '
+    frontSpeed = ' '
+    frontBrightness = ' '
+    strobePattern = ' '
+    strobeColor = ' '
+    strobeSpeed = ' '
+    strobeBrightness = ' '
+    mainDim = ' '
+    dutyCycle = ' '
+
+    def pushingMessage(self):
+
+    def pushingMessage(self):
+
+    def pushingMessage(self):
+
+
+class synchronisingMessage:
+    messageType = ' '
+    direction = ' '
+    beat_period_millis = 0
+    beat_distinctivness = 0
