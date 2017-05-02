6618b5be55f13ab9955c9dd52d6578ad856840d5
Simon Fritz <simon-fritz@gmx.de>
2017-05-02 15:24:40 -0700

    Folder Rework

--- LightControl/pixeltypes.py
+++ LightControl/pixeltypes.py
@@ -1,8 +1,25 @@
-class CRGB
-    def __init__():
+
+#http://fastled.io/docs/3.1/pixeltypes_8h_source.html
+
+class CRGB:
+        def __init__(self):
+        self.r = 0
+        self.g = 0
+        self.b = 0
+
+    def __init__(self, _r, _g, _b):
         self.r = _r
-        self.red = _red
         self.g = _g
-        self.green = _green
         self.b = _b
-        self.blue = _blue
+
+
+class CHSV:
+    def __init__(self):
+        self.h = 0
+        self.s = 0
+        self.v = 0
+
+    def __init__(self, _h, _s, _v):
+        self.h = _h
+        self.s = _s
+        self.v = _v
